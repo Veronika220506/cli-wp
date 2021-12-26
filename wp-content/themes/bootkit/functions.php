c0bcb365b708795c13c530bb2f945b921587fdca
Marichka <marichka.vyalik@gmail.com>
2021-12-26 13:21:37 +0200

    add changes

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -4,8 +4,8 @@ define('BOOTSTRAPTOPIC_DEV_MODE', false);
 
 // Includes
 include get_theme_file_path('includes/enqueue.php');
-
+include get_theme_file_path('includes/widgets.php');
 // Hooks
 add_action('wp_enqueue_scripts', 'bootkit_enqueue');
-
+add_action('widgets_init', 'bootkit_widgets');
 // Shortcodes
