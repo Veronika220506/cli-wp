83ec317726e3e396c7b78c73ef05dc0b20b9c80d
Marichka <marichka.vyalik@gmail.com>
2022-01-23 14:42:17 +0200

    add

--- wp-content/themes/bootkit/comments.php
+++ wp-content/themes/bootkit/comments.php
@@ -2,12 +2,11 @@
     return;
 }
 ?>
- <!-- Comments Form -->
- <div class="card my-4">
-                <h5 class="card-header">Leave a Comment:</h5>
-                <div class="card-body">
-                    
-                    <?php comment_form([
+<!-- Comments Form -->
+<div class="card my-4">
+    <h5 class="card-header">Leave a Comment:</h5>
+    <div class="card-body">
+        <?php comment_form([
     'comment_field' => '<div class="form-group">
     <textarea name="comment" cols="58" rows="3" class="form-control"></textarea>
 </div>',
@@ -34,35 +33,41 @@
 ]
 );
 ?>
-                </div>
-            </div>
+    </div>
+</div>
+<?php
+if (have_comments()) {?>
+<h5 class="comments-title"><span><?php comments_number();?></span></h5>
+<!-- Comments List -->
 
-           <!-- Comments List -->
+<?php
 
-       <?php
+    foreach ($comments as $comment) {
+        ?>
+<div class="media mb-4">
+    <div class="comment-meta">
+        <div class="comment-author vcard">
+            <span class="comment-avatar clearfix">
+                <?php echo get_avatar($comment, 60, '', '', ['class' => 'd-flex mr-3 rounded-circle']); ?>
+            </span>
+        </div>
+    </div>
+    <div class="comment-content clearfix">
+        <div class="comment-author">
+            <h5 class="mt-0"> <?php comment_author();?></h5>
+            <span><?php comment_date();?></span>
+        </div>
+        <div class="media-body">
+            <?php comment_text();?>
+        </div>
 
-foreach ($comments as $comment) {
-    ?>
-       <div class="media mb-4">
-           <div class="comment-meta">
-               <div class="comment-author vcard">
-                   <span class="comment-avatar clearfix">
-                       <?php echo get_avatar($comment, 60, '', '', ['class' => 'd-flex mr-3 rounded-circle']); ?>
-                   </span>
-               </div>
-           </div>
-           <div class="comment-content clearfix">
-               <div class="comment-author">
-                   <?php comment_author();?>
-                   <span><?php comment_date();?></span>
-               </div>
-               <div class="media-body">
-                   <?php comment_text();?>
-               </div>
-           </div>
-       </div>
-       <?php
+    </div>
+</div>
+<?php
 }
-the_comments_pagination();
+    the_comments_pagination();
+    ?>
+<?php
+}
 ?>
 <!-- .commentlist end -->
--- wp-content/themes/bootkit/single.php
+++ wp-content/themes/bootkit/single.php
@@ -4,46 +4,50 @@ get_header();
 <!-- Page Content -->
 <div class="container">
 
-    
 
     <!-- Marketing Icons Section -->
     <div class="row">
 
- <!-- Blog Entries Column -->
-      <div class="col-md-8">
-      
-        <?php if (have_posts()) {
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <?php if (have_posts()) {
     while (have_posts()) {
         the_post();
         global $post;
         $author_ID = $post->post_author;
         $author_URL = get_author_posts_url($author_ID);
         ?>
-         <!-- Title -->
-         <h1 class="mt-4"><?php the_title()?></h1>
+            <!-- Title -->
+            <h1 class="mt-4"><?php the_title()?></h1>
             <!-- Post category: -->
             <h2 class="mt-4"><?php the_category(" ")?></h2>
 
-           <!-- Date/Time -->
-           <p><?php the_time(get_option('date_format'));
+            <!-- Date/Time -->
+            <p><?php the_time(get_option('date_format'));
         echo " ";
         the_time(get_option('time_format'));?></p>
             <hr>
-          <!-- Author -->
-          <p class="lead">
+
+
+            <!-- Author -->
+            <p class="lead">
                 by
                 <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
             </p>
+            <hr>
+
             <!-- Preview Image -->
             <?php
 if (has_post_thumbnail()) {
             the_post_thumbnail("full", ["class" => "card-img-top"]);
         }
         ?>
-        <hr>
+            <hr>
 
-<!-- Post Content -->
-<?php the_content();
+
+            <!-- Post Content -->
+            <?php the_content();
+
         $defaults = array(
             'before' => '<div class="row justify-content-center align-items-center">' . __('Pages:'),
             'after' => '</div>',
@@ -51,12 +55,28 @@ if (has_post_thumbnail()) {
         wp_link_pages($defaults);
         edit_post_link();
         ?>
-<hr>
- <!-- Tag cloud -->
- <?php the_tags();?>
+            <!-- Post Author Info -->
+            <div class="card">
+                <div class="card-header">
+                    <strong>
+                        Posted by
+                        <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+                    </strong>
+                </div>
+                <div class="card-body">
+                    <div class="author-image">
+                        <?php echo get_avatar($author_ID, 90, '', false, ['class' => 'img-circle']); ?>
+                    </div>
+                    <?php echo nl2br(get_the_author_meta('description')); ?>
+                </div>
+            </div><!-- Post Single - Author End -->
+
             <hr>
-             <!-- Pagination -->
-             <ul class="pagination justify-content-center mb-4">
+            <!-- Tag cloud -->
+            <?php the_tags('', ', ');?>
+            <hr>
+            <!-- Pagination -->
+            <ul class="pagination justify-content-center mb-4">
                 <li class="page-item">
                     <?php previous_post_link();?>
                 </li>
@@ -64,21 +84,28 @@ if (has_post_thumbnail()) {
                     <?php next_post_link();?>
                 </li>
             </ul>
+
             <?php
-        if (comments_open() || get_comments_number()) {
+if (comments_open() || get_comments_number()) {
             comments_template();
         }
+        ?>
+            <?php
+}
+}
 ?>
 
-        <?php
-}
-}
-?>
-      </div>
+
+
+
+        </div>
+
         <?php get_sidebar();?>
     </div>
     <!-- /.row -->
     <hr>
+
+
 </div>
 <!-- /.container -->
 <?php
