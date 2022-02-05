5df6bc803c034048a915b900f9cbe4c7667d8ca3
Veronika22 <veronikakravchenko22@gmail.com>
2022-02-05 20:16:17 +0200

    mytaxonomies

--- wp-content/themes/bootkit/includes/custom-post-types.php
+++ wp-content/themes/bootkit/includes/custom-post-types.php
@@ -29,4 +29,32 @@ function bootkit_register_post_type_init()
         'taxonomies' => array( 'category','genre','director'  ),
     );
     register_post_type('movies', $args);
+    $labels = array(
+        'name' => 'Manga',
+        'singular_name' => 'Manga', // show in admin panel add->Movie
+        'add_new' => 'Add Manga',
+        'add_new_item' => 'Add new Manga', // <title>
+        'edit_item' => 'Edit Manga',
+        'new_item' => 'New Manga',
+        'all_items' => 'All Manga',
+        'view_item' => 'View Mangas',
+        'search_items' => 'Search Mangas',
+        'not_found' => 'Mangas not found.',
+        'not_found_in_trash' => 'Mangas not found in trash.',
+        'menu_name' => 'Manga',
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true, //for all users - true
+        'show_ui' => true, // show in admin panel
+        'has_archive' => true,
+        'menu_icon' => get_stylesheet_directory_uri() . '/img/function_icon.png', // иконка в меню
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 20,
+        'supports' => array('title', 'editor', 'comments', 'author', 'thumbnail'),
+        'taxonomies' => array( 'category','genre','author'  ),
+    );
+    register_post_type('manga', $args);
 }
--- wp-content/themes/bootkit/includes/taxonomies.php
+++ wp-content/themes/bootkit/includes/taxonomies.php
@@ -88,4 +88,91 @@ function create_movie_taxonomies()
         ),
     )
 );
+register_taxonomy('genre',
+        array('post'),
+        array(
+            'hierarchical' => true,
+            /* true: like categories, false: like tags,
+            default: false */
+            'labels' => array(
+                'name' => 'Manga genres',
+                'singular_name' => 'Genre',
+                'search_items' => 'Search genre',
+                'popular_items' => 'Popular genres',
+                'all_items' => 'All genres',
+                'parent_item' => 'Parent Genre',
+                'parent_item_colon' => 'Parent Genre',
+                'edit_item' => 'Edit genre',
+                'update_item' => 'Update genre',
+                'add_new_item' => 'Add new genre',
+                'new_item_name' => 'New Genre name',
+                'separate_items_with_commas' => 'Separate Genres with commas',
+                'add_or_remove_items' => 'Add or remove genre',
+                'choose_from_most_used' => 'Choose from the most used genres',
+                'menu_name' => 'Genre',
+            ),
+            'public' => true,
+            /* for all users - true */
+            'show_in_nav_menus' => true,
+            /* add to Menu */
+            'show_ui' => true,
+            /* show in admin panel */
+            'show_tagcloud' => true,
+            /* permit tagcloud */
+            'update_count_callback' => '_update_post_term_count',
+            /* callback-function to update the counter $object_type */
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'genre', // slug
+                'hierarchical' => true,
+
+            ),
+        )
+    );
+    register_taxonomy('author',
+    array('post'),
+    array(
+        'hierarchical' => false,
+        /* true: like categories, false: like tags,
+        default: false */
+        'labels' => array(
+            'name' => 'Mangas authors',
+            'singular_name' => 'Author',
+            'search_items' => 'Search author',
+            'popular_items' => 'Popular authors',
+            'all_items' => 'All authors',
+            'parent_item' => null,
+            'parent_item_colon' => null,
+            'edit_item' => 'Edit author',
+            'update_item' => 'Update author',
+            'add_new_item' => 'Add new  author',
+            'new_item_name' => 'New author name',
+            'separate_items_with_commas' => 'Separate Authors with commas',
+            'add_or_remove_items' => 'Add or remove author',
+            'choose_from_most_used' => 'Choose from the most used authors',
+            'menu_name' => 'Author',
+        ),
+        'public' => true,
+
+        /* for all users - true */
+        'show_in_nav_menus' => true,
+
+        /* add to Menu */
+        'show_ui' => true,
+
+        /* add User Interface */
+        'show_tagcloud' => true,
+
+        /* permit tagcloud */
+        'update_count_callback' => '_update_post_term_count',
+
+        /* callback-function to update the counter $object_type */
+        'query_var' => true,
+
+        'rewrite' => array(
+            'slug' => 'author', // slug
+            'hierarchical' => true,
+        ),
+    )
+);
 }
