524a009a92a9f3625ebf86d63455f797e1f23e33
Veronika22 <veronikakravchenko22@gmail.com>
2022-04-17 15:40:30 +0300

    add shortcodes

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -32,3 +32,13 @@ add_action('customize_register', 'bootkit_customize_register');
 
 
 // Shortcodes
+function site_url_shortcode($atts)
+{
+    return site_url();
+}
+add_shortcode('myurl', 'site_url_shortcode');
+function time_shortcode($atts)
+{
+    return time_to_post_content($content);
+}
+add_shortcode('time_to_post', 'time_shortcode');
