b0c58d3a1f8a2f285c7d3e020e19f2096438ac45
Veronika22 <veronikakravchenko22@gmail.com>
2022-04-17 17:18:50 +0300

    add changes

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -52,3 +52,68 @@ add_shortcode('time_to_post', 'time_shortcode');
 //     echo $summa;
 //     die;
 // }
+if (function_exists('register_sidebars')) {
+    register_sidebar(array(
+        'name' => 'Sidebar Area',
+        'before_widget' => '<div id="%1$s" class="backgroundlist %2$s"><div class="listtitle">',
+        'after_widget' => '</div></div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2></div><div class="contentbox">',
+    ));
+}
+//------------Widget-------------
+/* Виджет Bootkit Widget */
+class bootkit_widget extends WP_Widget
+{
+    public function __construct()
+    {
+        parent::__construct(
+            // widget ID
+            'bootkit_widget',
+            // widget name
+            __('Simple widget', ' bootkit_widget_domain'),
+            // widget description
+            array('description' => __('Simple widget bootkit', 'bootkit_widget_domain'))
+        );
+    }
+
+    public function widget($args, $instance)
+    {
+        $title = apply_filters('widget_title', $instance['title']);
+        echo $args['before_widget'];
+        if (!empty($title)) {
+            echo $args['before_title'] . $title . $args['after_title'];
+        }
+        echo __('Hello, bootkit', 'bootkit_widget_domain');
+    }
+
+    public function form($instance)
+    {
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        } else {
+            $title = __('Default header', 'bootkit_widget_domain');
+        }
+        ?>
+
+<p>
+    <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:');?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+        name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+</p>
+<?php
+}
+
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        return $instance;
+    }
+}
+
+function bootkit_register_widget()
+{
+    register_widget('bootkit_widget');
+}
+add_action('widgets_init', 'bootkit_register_widget');
